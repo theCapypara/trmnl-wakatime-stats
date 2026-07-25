322d2c3eeff092f8036e7f3339bbf403b1285b0d
Marco "Capypara" Köpcke <hello@capypara.de>
2026-07-26 00:57:21 +0200

    show error screen if current data is not available

--- src/transform.py
+++ src/transform.py
@@ -1,6 +1,15 @@
 def run(input):
-    # Make sure the Payload doesn't exceed the limit
-    if "data" in input:
+    if "data" not in input:
+        return {"errors": [
+            f"WakaTime did not return any data."
+        ]}
+    elif input["data"]["status"] != "ok":
+        return {"errors": [
+            f"WakaTime does not have the stats calculated yet. Current status: {input["data"]["status"]} "
+            f"({input["data"]["percent_calculated"] if "percent_calculated" in input["data"] else 0}%)."
+        ]}
+    else:
+        # Make sure the Payload doesn't exceed the limit
         for key in ("dependencies", "operating_systems", "projects", "machines"):
             if key in input["data"]:
                 input["data"][key] = input["data"][key][:75]
